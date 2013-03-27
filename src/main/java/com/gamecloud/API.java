796107eb76260e026bba91092a08837770e1d1fe
Mateusz Korszun <mkorszun@gmail.com>
2013-03-27 20:43:55 +0100

    Extend api

--- src/main/java/com/gamecloud/API.java
+++ src/main/java/com/gamecloud/API.java
@@ -15,36 +15,68 @@ public class API {
         new APIClient(path("/developer")).post1(developer);
     }
 
-    public static JSONObject readDeveloper(String id) throws APIException {
-        return new APIClient(path("/developer/"+id)).get();
+    public static JSONObject readDeveloper1(String id, String password) throws APIException {
+        return new APIClient(id, password, path("/developer/"+id)).get();
     }
 
-    public static void updateDeveloper(String id, JSONObject developer) throws APIException {
-        new APIClient(path("/developer/"+id)).put(developer);
+    public static JSONObject readDeveloper2(String id, String token) throws APIException {
+        return new APIClient(token, path("/developer/"+id)).get();
     }
 
-    public static void deleteDeveloper(String id) throws APIException {
-        new APIClient(path("/developer/"+id)).delete();
+    public static void updateDevelope1r(String id, String password, JSONObject developer) throws APIException {
+        new APIClient(id, password, path("/developer/"+id)).put(developer);
     }
 
-    public static void createGame(String developerId, JSONObject game) throws APIException {
-        new APIClient(path("/developer/"+developerId+"/game")).post1(game);
+    public static void updateDeveloper2(String id, String token, JSONObject developer) throws APIException {
+        new APIClient(token, path("/developer/"+id)).put(developer);
     }
 
-    public static JSONObject readGame(String developerId) throws APIException {
-        return new APIClient(path("/developer/"+developerId+"/game")).get();
+    public static void deleteDeveloper1(String id, String password) throws APIException {
+        new APIClient(id, password, path("/developer/"+id)).delete();
     }
 
-    public static JSONObject readGame(String developerId, String gameKey) throws APIException {
-        return new APIClient(path("/developer/"+developerId+"/game/"+gameKey)).get();
+    public static void deleteDeveloper2(String id, String token) throws APIException {
+        new APIClient(token, path("/developer/"+id)).delete();
     }
 
-    public static void updateGame(String developerId, String gameKey, JSONObject game) throws APIException {
-        new APIClient(path("/developer/"+developerId+"/game/"+gameKey)).put(game);
+    public static void createGame1(String id, String password, JSONObject game) throws APIException {
+        new APIClient(id, password, path("/developer/"+id+"/game")).post1(game);
     }
 
-    public static void deleteGame(String developerId, String gameKey) throws APIException {
-        new APIClient(path("/developer/"+developerId+"/game/"+gameKey)).delete();
+    public static void createGame2(String id, String token, JSONObject game) throws APIException {
+        new APIClient(token, path("/developer/"+id+"/game")).post1(game);
+    }
+
+    public static JSONObject readGame1(String id, String password) throws APIException {
+        return new APIClient(id, password, path("/developer/"+id+"/game")).get();
+    }
+
+    public static JSONObject readGame2(String id, String token) throws APIException {
+        return new APIClient(token, path("/developer/"+id+"/game")).get();
+    }
+
+    public static JSONObject readGame1(String id, String password, String gameKey) throws APIException {
+        return new APIClient(id, password, path("/developer/"+id+"/game/"+gameKey)).get();
+    }
+
+    public static JSONObject readGame2(String id, String token, String gameKey) throws APIException {
+        return new APIClient(token, path("/developer/"+id+"/game/"+gameKey)).get();
+    }
+
+    public static void updateGame1(String id, String password, String gameKey, JSONObject game) throws APIException {
+        new APIClient(id, password, path("/developer/"+id+"/game/"+gameKey)).put(game);
+    }
+
+    public static void updateGame2(String id, String token, String gameKey, JSONObject game) throws APIException {
+        new APIClient(token, path("/developer/"+id+"/game/"+gameKey)).put(game);
+    }
+
+    public static void deleteGame1(String id, String password, String gameKey) throws APIException {
+        new APIClient(id, password, path("/developer/"+id+"/game/"+gameKey)).delete();
+    }
+
+    public static void deleteGame2(String id, String token, String gameKey) throws APIException {
+        new APIClient(token, path("/developer/"+id+"/game/"+gameKey)).delete();
     }
 
     private static String path(String resource){
